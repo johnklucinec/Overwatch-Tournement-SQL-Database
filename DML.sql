3fd101c06ad9fc788dfdae1152826f3d1ea8f046
DaddyJuanito <jsklucinec@gmail.com>
2024-02-13 22:47:19 -0500

    added team query

--- DML.sql
+++ DML.sql
@@ -5,6 +5,53 @@
 
 -- Query to retrieve team information along with players, their roles, and ranks
 
+-- Query to retrieve all the teams information, including Name, AverageRank, MMR, FormationDate, and Players(amount)
+SELECT 
+    t.teamName AS Name,
+    CONCAT(
+        CASE
+            WHEN AVG(highest_mmr.max_mmr) >= 4500 THEN 'Champion'
+            WHEN AVG(highest_mmr.max_mmr) >= 4000 THEN 'Grandmaster'
+            WHEN AVG(highest_mmr.max_mmr) >= 3500 THEN 'Master'
+            WHEN AVG(highest_mmr.max_mmr) >= 3000 THEN 'Diamond'
+            WHEN AVG(highest_mmr.max_mmr) >= 2500 THEN 'Platinum'
+            WHEN AVG(highest_mmr.max_mmr) >= 2000 THEN 'Gold'
+            WHEN AVG(highest_mmr.max_mmr) >= 1500 THEN 'Silver'
+            WHEN AVG(highest_mmr.max_mmr) >= 1000 THEN 'Bronze'
+            ELSE 'Unranked'
+        END,
+        ' ',
+        CASE
+            WHEN AVG(highest_mmr.max_mmr) % 500 < 100 THEN '5'
+            WHEN AVG(highest_mmr.max_mmr) % 500 < 200 THEN '4'
+            WHEN AVG(highest_mmr.max_mmr) % 500 < 300 THEN '3'
+            WHEN AVG(highest_mmr.max_mmr) % 500 < 400 THEN '2'
+            WHEN AVG(highest_mmr.max_mmr) % 500 < 500 THEN '1'
+        END
+    ) AS AverageRank,
+    FLOOR(AVG(highest_mmr.max_mmr)) AS MMR,
+    DATE_FORMAT(t.formationDate, '%Y-%m-%d') AS FormationDate,
+    COUNT(DISTINCT playerID) AS Players
+FROM 
+    Teams t
+JOIN 
+    (
+        SELECT 
+            tp.teamID,
+            tp.playerID,
+            MAX(rk.mmr) AS max_mmr
+        FROM 
+            TeamPlayers tp
+        JOIN 
+            PlayerRoles pr ON tp.playerID = pr.playerID AND tp.roleID = pr.roleID
+        JOIN 
+            Ranks rk ON pr.rankID = rk.rankID
+        GROUP BY 
+            tp.teamID, tp.playerID
+    ) AS highest_mmr ON t.teamID = highest_mmr.teamID
+GROUP BY 
+    t.teamID;
+
 -- Query to retrieve tournament information along with participating teams
 
 
