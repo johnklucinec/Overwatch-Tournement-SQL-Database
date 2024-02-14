bba4c5591f1f6fbfa35a9a03426ad84400ca3980
RemyTroy <107515216+RemyTroy@users.noreply.github.com>
2024-02-13 21:21:03 -0500

    Update DML.sql

--- DML.sql
+++ DML.sql
@@ -1,1 +1,34 @@
+-- SELECT Queries:
 
+
+-- Query to retrieve player information along with their roles and ranks
+
+-- Query to retrieve team information along with players, their roles, and ranks
+
+-- Query to retrieve tournament information along with participating teams
+
+
+
+
+-- INSERT Queries: 
+
+-- Query to add a new player
+-- Query to add a new team
+-- Query to add a new tournament
+-- Query to add a new player's role
+-- Query to add a player to a team
+-- Query to add a team to a tournament
+
+-- UPDATE Queries:
+
+-- Query to update player email
+-- Query to update team formation date
+-- Query to update tournament end date
+-- Query to update player's role and rank
+
+
+
+-- DELETE Queries
+-- Query to remove a player from a team
+-- Query to remove a team from a tournament
+-- Query to remove a player's role
